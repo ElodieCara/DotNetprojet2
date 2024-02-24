c18e22f2ae9349a7825c544e622735c0e719292d
E Cara <elodie.caradeuc@gmail.com>
2024-02-25 00:08:13 +0100

    Commit "Correction traduction messages d'erreur"

--- P2FixAnAppDotNetCode/Controllers/OrderController.cs
+++ P2FixAnAppDotNetCode/Controllers/OrderController.cs
@@ -1,5 +1,7 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.Extensions.Localization;
 using P2FixAnAppDotNetCode.Models;
 using P2FixAnAppDotNetCode.Models.Services;
@@ -43,7 +45,7 @@ namespace P2FixAnAppDotNetCode.Controllers
                     if (product != null && product.Stock >= line.Quantity)
                     {
                         // Déduire la quantité commandée de la quantité en stock du produit
-                        product.Stock -= line.Quantity;
+                       // product.Stock -= line.Quantity; 
 
                         // Mettre à jour le produit dans la base de données
                         _productService.UpdateProduct(product);
@@ -61,6 +63,31 @@ namespace P2FixAnAppDotNetCode.Controllers
             }
             else
             {
+                // Traduire les erreurs de validation du modèle
+                foreach (var key in ModelState.Keys)
+                {
+                    var modelStateEntry = ModelState[key];
+                    var translatedErrors = new List<ModelError>();
+
+                    foreach (var error in modelStateEntry.Errors)
+                    {
+                        if (!string.IsNullOrEmpty(error.ErrorMessage))
+                        {
+                            var translatedErrorMessage = _localizer[error.ErrorMessage];
+                            var translatedError = new ModelError(translatedErrorMessage);
+                            translatedErrors.Add(translatedError);
+                        }
+                    }
+
+                    // Supprimer les erreurs existantes
+                    modelStateEntry.Errors.Clear();
+
+                    // Ajouter les nouvelles erreurs traduites
+                    foreach (var translatedError in translatedErrors)
+                    {
+                        modelStateEntry.Errors.Add(translatedError);
+                    }
+                }
                 return View(order);
             }
         }
